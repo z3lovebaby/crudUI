4b5578ea2d77c4c35c04530a5b8934b94ac50dac
DIEPPV <dieppv.evnit@evn.com.vn>
2024-01-18 09:40:36 +0700

    f commit

---
+++ src/app/features/examscore/model/update-examscore-request.model.ts
@@ -0,0 +1,6 @@
+export interface UpdateExamScoreRequest {
+  id:string;
+  score:number;
+  stId:string;
+  cId:string;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiBaseUrl: 'http://10.1.119.52:8090'
+};
